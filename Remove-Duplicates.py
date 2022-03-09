da1d8ad21f58a1ca42c68bf10748aca2a07e4b6d
Pranit Puri <65455865+PRAN20@users.noreply.github.com>
2022-03-09 23:23:02 +0530

    Create Remove-Duplicates.py

---
+++ Remove-Duplicates.py
@@ -0,0 +1,26 @@
+class Solution:
+    def deleteDuplicates(self, head: ListNode) -> ListNode:
+        # sentinel
+        sentinel = ListNode(0, head)
+
+        # predecessor = the last node 
+        # before the sublist of duplicates
+        pred = sentinel
+        
+        while head:
+            # if it's a beginning of duplicates sublist 
+            # skip all duplicates
+            if head.next and head.val == head.next.val:
+                # move till the end of duplicates sublist
+                while head.next and head.val == head.next.val:
+                    head = head.next
+                # skip all duplicates
+                pred.next = head.next 
+            # otherwise, move predecessor
+            else:
+                pred = pred.next 
+                
+            # move forward
+            head = head.next
+            
+        return sentinel.next
